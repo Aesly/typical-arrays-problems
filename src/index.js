4cc13ad5ad777f75d67896cc8d79228fabd9227b
Vladislav Kurchanov <dikdamned@gmail.com>
2020-10-03 21:57:03 +0300

    init: do task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) { 
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } 
+else {
+    var min = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i]; 
+    }  
+    return min;
+} 
 }
+    
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } 
+else {
+    var max = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (max < array[i]) max = array[i];
+        
+    } 
+    return max;
+}
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } 
+else {
+        for (var i=0,sum=0;i<array.length;i++) {
+          sum += array[i];
+        }
+        return sum / i;
+}
 }
